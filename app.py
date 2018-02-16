fc5d9af6a0201634bfb4e059c8b2779a37292f0b
Ahmad Fahmi <afahmi3@yahoo.co.id>
2018-02-16 13:05:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import sys
-from argparse import ArgumentParser
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -12,14 +11,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-# Import command.py, isinya modul keyword
-# from command import resolve
-
-
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('IF+3kDg6rVn7d/mqWaoxV6fgKPdcoc8741H10GDiIu60d4Zdvm4BMEtJOd/zllm2RKas/nOb+rgpwZL6R0evIfCJRWD2trc5FO6Ju1Lj+G4V84gJr+JK/jhZmIk5v9lmUaxZbhb7BmBc6r7ycnO/MwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('a0cd332e19192d201ca07610f32cbf8a')
+YOUR_CHANNEL_ACCESS_TOKEN = 'IF+3kDg6rVn7d/mqWaoxV6fgKPdcoc8741H10GDiIu60d4Zdvm4BMEtJOd/zllm2RKas/nOb+rgpwZL6R0evIfCJRWD2trc5FO6Ju1Lj+G4V84gJr+JK/jhZmIk5v9lmUaxZbhb7BmBc6r7ycnO/MwdB04t89/1O/w1cDnyilFU='
+YOUR_CHANNEL_SECRET = 'a0cd332e19192d201ca07610f32cbf8a'
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
@@ -42,12 +39,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #text = event.message.text
-    #resolve(line_bot_api, event, text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
