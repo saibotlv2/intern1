1130bbf079659b1991166e6ece0347dcac0eec97
Ahmad Fahmi <afahmi3@yahoo.co.id>
2018-02-16 17:59:48 +0700

    Update Intern.py

--- Commands/Intern.py
+++ Commands/Intern.py
@@ -7,54 +7,38 @@ from linebot.models import (
 # Showing internship info
 def intern_info(line_api, event, text):
     result = ""
+    company = open("../intern_data.json", "r")
+    company_data = json.load(company)
 
-    if text == 'list':
+    if text.lower() == 'list':
         result = "Cara pakai: ketik {!intern<spasi>nama perusahaan}\n"\
                  "contoh: !intern bukalapak\n\n"\
-                 "Company lists:\n"\
-                 "- bukalapak\n"\
-                 "- tokopedia\n"\
-                 "- kata.ai\n"\
-                 "- go-life\n"\
-
-    elif text == 'bukalapak':
-        result = "Internship / Full Time at Bukalapak\n\n"\
-                 "Durasi minimal 2,5 bulan\n\n"
-
-        with urllib.request.urlopen("https://careers.bukalapak.com/jobs") as url:
-            data_bl = json.loads(url.read().decode())
-
-        for i in range(len(data_bl)):
-            for j in range(len(data_bl[i]["list"])):
-                if data_bl[i]["list"][j]["type"] == "Internship":
-                    result = result + ('Role: ' + data_bl[i]["list"][j]['title'] + '\n')
-                    result = result + ('url: ' + data_bl[i]["list"][j]['url'] + '\n')
-
-    elif text == 'tokopedia':
-        result = "Internship / Full Time at Tokopedia\n\n"\
-                 "Durasi minimal 2,5 bulan\n"\
-                 "Link: https://www.tokopedia.com/careers/jobs/ \n"
-
-    elif text == 'kata.ai':
-        result = "Internship at Kata.ai\n\n"\
-                 "Kirim cv ke jobs@kata.ai\n"\
-                 "cc: rizqi@kata.ai \n"
-
-    elif text == 'go-life':
-        result = "Internship / Full Time at GO-LIFE\n\n"\
-                 "Roles:\n"\
-                 "1. Product Engineer\n"\
-                 "2. UX Research\n\n"\
-                 "contact: adi.p@go-jek.com\n"
-
-    elif text == 'kata.ai':
-        result = "Kirim cv ke jobs@kata.ai\n"\
-                 "cc: rizqi@kata.ai \n"
+                 "Company lists:\n\n"
+        
+        for i in range(len(company_data)):
+            result = result + str(i) + '. ' + company_data[i]["company"] + '\n'
     
+    else:
+        for i in range(len(company_data)):
+            if text.lower() == company_data[i]["company"]:
+                for j in range(len(company_data[i]["content"])):
+                    result = result + company_data[i]["content"][j] + '\n'
+                result = result + '\n'
+
+                if text.lower() == 'bukalapak':
+                    with urllib.request.urlopen("https://careers.bukalapak.com/jobs") as url:
+                        data = json.loads(url.read().decode())
+
+                    for i in range(len(data)):
+                        for j in range(len(data[i]["list"])):
+                            if data[i]["list"][j]["type"] == "Internship":
+                                result = result + ('Category: ' + data[i]["category"] + '\n')
+                                result = result + ('Role: ' + data[i]["list"][j]['title'] + '\n')
+                                result = result + ('url: ' + data[i]["list"][j]['url'] + '\n\n')
+
     line_api.reply_message(
         event.reply_token, TextSendMessage(text=result)
     )
-
 
 # Showing internship tips
 def intern_tips(line_api, event, text):
