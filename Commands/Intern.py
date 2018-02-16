d1f4ca767b13f0f2a31cb0dabb4a031d7b77030a
Ahmad Fahmi <afahmi3@yahoo.co.id>
2018-02-16 21:31:03 +0700

    Update Intern.py

--- Commands/Intern.py
+++ Commands/Intern.py
@@ -4,20 +4,19 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
-
 # Showing internship info
 def intern_info(line_api, event, text):
     result = ""
-    # company = open("intern_data.json", "r")
-    # company_data = json.load(company)
+    company = open("../intern_data.json", "r")
+    company_data = json.load(company)
 
     if text.lower() == 'list':
         result = "Cara pakai: ketik {!intern<spasi>nama perusahaan}\n"\
                  "contoh: !intern bukalapak\n\n"\
                  "Company lists:\n\n"
         
-        # for i in range(len(company_data)):
-        #     result = result + str(i+1) + '. ' + company_data[i]["company"] + '\n'
+        for i in range(len(company_data)):
+            result = result + str(i) + '. ' + company_data[i]["company"] + '\n'
     
     else:
         for i in range(len(company_data)):
@@ -30,27 +29,26 @@ def intern_info(line_api, event, text):
             with urllib.request.urlopen("https://careers.bukalapak.com/jobs") as url:
                 data = json.loads(url.read().decode())
 
-            for j in range(len(data)):
-                for k in range(len(data[j]["list"])):
-                    if data[j]["list"][k]["type"] == "Internship":
-                        result = result + ('Category: ' + data[j]["category"] + '\n')
-                        result = result + ('Role: ' + data[j]["list"][k]['title'] + '\n')
-                        result = result + ('url: ' + data[j]["list"][k]['url'] + '\n\n')
+            for i in range(len(data)):
+                for j in range(len(data[i]["list"])):
+                    if data[i]["list"][j]["type"] == "Internship":
+                        result = result + ('Category: ' + data[i]["category"] + '\n')
+                        result = result + ('Role: ' + data[i]["list"][j]['title'] + '\n')
+                        result = result + ('url: ' + data[i]["list"][j]['url'] + '\n\n')
 
     line_api.reply_message(
         event.reply_token, TextSendMessage(text=result)
     )
 
-
 # Showing internship tips
 def intern_tips(line_api, event, text):
     result = ""
 
-    if text.lower() == 'cv':
+    if text == 'cv':
         result = "Guide: https://www.careercup.com/resume\n" \
                  "Advice (good to read): https://codeburst.io/competitive-programming-label-from-an-employers-perspective-5209d6843e2a\n" \
                  "Lumayan penting (terutama kalau ga ikut lomba/aktif di komunitas teknologi) : Side projects (project yang dikerjain sendiri, bukan dari tugas kuliah)"
-    elif text.lower() == 'interview':
+    elif text == 'interview':
         result = "Mayoritas ngetes kemampuan problem solving menggunakan ilmu dari materi algoritma dan struktur data\n" \
                  "Contoh soal: Implementasikan queue menggunakan 2 buah stack, dengan queue memiliki 2 method, yaitu enqueue dan dequeue\n" \
                  "Beberapa perusahaan ngetes hal lain juga, contoh: SQL, System Design, CSS, Common technologies (co: LAMP stack)\n" \
